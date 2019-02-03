cc919bc9d6d7c8d2e2bad99e2d68aeccdae368ca
katkinso <katyratkinson@yahoo.com>
2019-02-03 09:37:15 -0800

    checkpoint-3-create-chat-rooms: Complete

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,7 +5,8 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ""
     };
 
     this.roomsRef = this.props.firebase.database().ref("rooms");
@@ -19,13 +20,66 @@ class RoomList extends Component {
     });
   }
 
+  handleChange(e){
+    const newRoomName = e.target.value;
+    this.setState({newRoomName:newRoomName})
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    const newRoomName = this.state.newRoomName.trim()
+
+    if ((!newRoomName) || this.findRoomByName(newRoomName)) {
+      return;
+    }
+
+    const key = this.roomsRef.push({
+      name: newRoomName
+    }).key;
+
+    const newRoom = {
+      name: newRoomName,
+      key: key
+    }
+
+    this.setState({
+      rooms: this.state.rooms.concat(newRoom),
+      newRoomName: ""
+    })
+
+  }
+
+  findRoomByName(roomName){
+    return this.state.rooms.find(room => roomName.toLowerCase() === room.name.toLowerCase())
+  }
+
+
   render() {
     return (
       <div>
         <h1>Available Chat Rooms</h1>
-        {this.state.rooms.map((room, key) => {
-          return <p key={key}>{room.name}</p>;
+        {this.state.rooms.map((room) => {
+          return <p key={room.key}>{room.name}</p>;
         })}
+
+        <form onSubmit={e => this.createRoom(e)}>
+
+          <div className="input-group mb-3">
+            <input type="text"
+              className="form-control"
+              placeholder="Room Name"
+              aria-label="Room Name"
+              aria-describedby="button-roomname"
+              value={this.state.newRoomName}
+              onChange={e => this.handleChange(e)}
+            />
+            <div className="input-group-append">
+              <input className="btn btn-primary" type="submit" id="button-roomname" value="CREATE ROOM" />
+            </div>
+          </div>
+
+        </form>
+
       </div>
     );
   }
